d3128e0d2af73951bd531afda348a12c1776fa40
CatOptimist <naturerainbowstar@gmail.com>
2020-03-10 20:59:06 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 class Main {
 	public static void main(String args[]) {
 		ArrayList fruit = new ArrayList();
-		fruit.add("banana");
-		fruit.add("apple");
-        fruit.add("orange");
+		fruit.add("banana"); // Index 0
+		fruit.add("apple"); // Index 1
+        fruit.add("orange"); // Index 2
         fruit.add(fruit.remove(1));
-        fruit.set(1, fruit.remove(2));
-        System.out.println(fruit);
+        fruit.set(1, fruit.remove(2)); // Removes "orange"
+        System.out.println(fruit); // Prints out [banana, apple]
 	}
 }
